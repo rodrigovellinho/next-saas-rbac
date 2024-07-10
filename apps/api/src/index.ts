88fde8665a56334231ad00fd1e8fe1719f064666
RodrigoKloeckner <rodrigo.kloeckner@gmail.com>
2024-07-10 12:39:22 -0300

    Setup Inicial Backend
    Setup Fastify & Setup BD & Rota cadastro usuário & Seed BD

--- apps/api/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { defineAbilityFor, projectSchema } from '@saas/auth'
-
-const project = projectSchema.parse({ id: 'project-id', ownerId: 'user-id' })
-
-const ability = defineAbilityFor({ role: 'ADMIN', id: 'user-id' })
-
-const userCanInvite = ability.can('delete', project)
-
-console.log(userCanInvite)
